fe17de0c712a8ed9687fa7160c2a40409fb187b3
Костя Кузнецов <kuznetsovkocty555@gmail.com>
2023-03-20 18:50:41 +0500

    Revert "тестирование"
    This reverts commit eece3e540851561e9b61f157c17112da7bfdda9a.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -91,22 +90,12 @@
             this.label2.Text = "Резервная копия";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(74, 173);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 21);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -127,7 +116,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button4;
     }
 }
 
