a3cc0f02be6c71f4f1ea1f8c9d8d3b8350af8d25
kuzne <kuzne@LAPTOP-SISD6O8C>
2023-03-20 18:38:03 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсовой_проект
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            form2 = new Test();
+            form3 = new Settings();
+        }
+        Test form2;
+        Settings form3;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
